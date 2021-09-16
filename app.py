01a45d3eea0d1d2f26dcd36de41b5eb55c444f19
Huan Zhang <31784445+Kristin33@users.noreply.github.com>
2021-09-16 12:14:26 +0800

    introduction page

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import numpy as np
 import music21 as m2
 from utils import *
 
+from pages.introduction import introduction_page
 from pages.melody import melody_page
 from pages.development import development_page 
 from pages.structure import structure_page 
@@ -26,13 +27,15 @@ author_credits = st.beta_container()
 
 
 with meta_input:
-    st.session_state.task = st.sidebar.selectbox('选择任务', ['主旋律', '发展旋律', '结构', '和弦', '后期'])
+    st.session_state.task = st.sidebar.selectbox('选择任务', ['介绍', '主旋律', '发展旋律', '结构', '和弦', '后期'])
 
     st.sidebar.header('参数选择') 
 
-    st.session_state.time_signature = st.sidebar.selectbox('选择拍号', ['2/4', '3/4', '4/4'],
+    time_signature = st.sidebar.selectbox('选择拍号', ['2/4', '3/4', '4/4'],
+                                key="time_signature",
                                 on_change=modify_mc)
-    st.session_state.key_signature = st.sidebar.selectbox('选择调号', ['C大调', 'G大调', 'D大调', 'A大调', 'E大调'],
+    key_signature = st.sidebar.selectbox('选择调号', ['C大调', 'G大调', 'D大调', 'A大调', 'E大调'],
+                                key="key_signature",
                                 on_change=modify_mc)
     st.session_state.n_measures = st.sidebar.slider('主旋律小节数量 ', min_value = 4, max_value= 12, value= 8, step=2)
     st.sidebar.slider('速度 ', min_value = 60, max_value= 180, value= 120, step=4,
@@ -44,7 +47,9 @@ with meta_input:
 
 
 with playground:
-    if st.session_state.task == '发展旋律':
+    if st.session_state.task == "介绍":
+        introduction_page()
+    elif st.session_state.task == '发展旋律':
         development_page()
     elif st.session_state.task == '结构':
         structure_page()
